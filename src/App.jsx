ff5824a0a03679d9043517e6ff52c042689a9992
Nethmina Lakshan Herath <nethminalakshan2018@gmail.com>
2026-03-15 23:43:43 +0530

    v1

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <main className="wrap">
+      <p className="tag">Simple React + Vite App</p>
+      <h1>Nethmina Lakshan</h1>
+      <p className="text">Portfolio starter is ready. Edit this page and add your projects.</p>
+      <button type="button">Get Started</button>
+    </main>
+  );
+}
+
+export default App;
